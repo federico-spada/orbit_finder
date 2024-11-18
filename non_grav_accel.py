23d13e58ec556696fdc58ae2cd134d73ebe58171
Federico Spada <federico.spada@gmail.com>
2024-11-18 09:16:37 +0100

    Minor fixes in non_grav_accel.py

--- non_grav_accel.py
+++ non_grav_accel.py
@@ -8,13 +8,12 @@ alpha, r0, m, n, k = 0.1113, 2.808, 2.15, 5.093, 4.6142
 
 
 ### FUNCTION IMPLEMENTING THE NON-GRAVITATIONAL ACCELERATION MODEL ------------
-# reorganized for clarity; fitting of DT will have to be added back later
 def NonGravAccel(r_, v_, parms_):
-    # cross product matrix 
+    # >>> cross product matrix (cf. Montenbruck & Gill 2000) 
     def X(w_):
-        return np.array([ [0., -w_[2], +w_[1]], 
-                          [+w_[2], 0., -w_[0]], 
-                          [-w_[1], +w_[0], 0.] ])
+        wx, wy, wz = w_
+        return np.array([ [0., -wz, +wy], [+wz, 0., -wx], [-wy, +wx, 0.] ])
+    # <<<
     r = np.linalg.norm(r_)
     h_ = np.cross(r_, v_)
     h = np.linalg.norm(h_) 
@@ -32,8 +31,7 @@ def NonGravAccel(r_, v_, parms_):
     du2dv = -X(u1_) @ du3dv
     dudr = np.array([du1dr, du2dr, du3dr])
     dudv = np.array([du1dv, du2dv, du3dv])
-    ## deal with symmetric case first: n_p ≤ 3
-    # Marsden+73 g(r) function, without delay parameter
+    # Marsden+73 g(r) function
     g = alpha * (r/r0) ** -m * (1. + (r/r0) ** n ) ** -k
     dgdr = -g/r * (m + k*n/(1 + (r0/r) ** n) )
     # construct acceleration vector and derivatives wrt Ai
@@ -42,12 +40,11 @@ def NonGravAccel(r_, v_, parms_):
     dadpNG = np.zeros((3, n_p))
     dadrNG = np.zeros((3,3))
     dadvNG = np.zeros((3,3))
+    # note numerical factors to have Ai's in units of 10^-8 au/d^2 
     for i in range(min(n_p, 3)):
         Ai = parms_[i] * 1e-8
         aNG_       += g * Ai * u_[:,i]
         dadpNG[:,i] = g * u_[:,i] * 1e-8
         dadrNG     += g * Ai * dudr[i] + dgdr * Ai * np.outer(u_[:,i], u1_)
         dadvNG     += g * Ai * dudv[i]
-    ## if n_p = 4, account for emission peak delay parameter tau
-    # !!! TBD !!!
     return aNG_, dadrNG, dadvNG, dadpNG
